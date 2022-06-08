9abe758f30c3be4a00a7361a0843b283e8261eb3
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-08 15:20:18 -0300

    Intro to lists and teleports

---
+++ src/views/ModalsView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="modals">
+    <h1>Modals</h1>
+    <button @click="showModal = true">Show modal</button>
+
+    <teleport to=".modals-container">
+      <div class="modal" v-if="showModal">
+        <h1>This is a modal</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ut quisquam quidem accusamus, dolorem
+          fugit
+          rem minima voluptatem eaque mollitia necessitatibus dolores, numquam omnis quibusdam eius corrupti perferendis
+          dolorum. Maxime.</p>
+        <button @click="showModal = false">Hide modal</button>
+      </div>
+    </teleport>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const showModal = ref(false)
+</script>
+
+<style>
+.modal {
+  background: beige;
+  padding: 10px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+
+.modal p,
+.modal h1 {
+  color: black;
+}
+</style>
