b84243fb3fb1239bc5986ec5e3e2290c06f76fa7
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-08 13:28:08 -0300

    Simple counter implementation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
+<template>
+  <div class="home">
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import { ref } from 'vue';
+
+const counter = ref(0);
+
+const increaseCounter = () => {
+  counter.value++
+}
+
+const decreaseCounter = () => {
+  counter.value--
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
